27be7b34559c216afa81113a7598c1fb69d3f1a1
valtermro <valtermro@outlook.com>
2020-12-10 19:30:12 -0300

    Add project files

---
+++ src/Xapu.Extensions.Selects/SelectExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Xapu.Extensions.Selects
+{
+    public static partial class SelectExtensions
+    {
+        public static bool IsTheTruth(this object source)
+        {
+            return true;
+        }
+    }
+}
---
+++ test/Xapu.Extensions.Selects.Tests/TypeMapping.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Xapu.Extensions.Selects.Tests
+{
+    public class TypeMapping
+    {
+        [Fact]
+        public void Passes()
+        {
+            Assert.True(42.IsTheTruth());
+        }
+    }             
+}
