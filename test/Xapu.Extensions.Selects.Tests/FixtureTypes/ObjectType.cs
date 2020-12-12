116326bd053ffba75f6764bd48694021039b931a
valtermro <valtermro@outlook.com>
2020-12-12 13:00:42 -0300

    ObjectType mapping

---
+++ test/Xapu.Extensions.Selects.Tests/FixtureTypes/ObjectType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Xapu.Extensions.Selects.Tests.FixtureTypes
+{
+    public class ObjectType
+    {
+        public Guid Id { get; set; }
+        public ObjectType Parent { get; set; }
+    }
+
+    public class WithIdView
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class ShallowObjectView : WithIdView
+    {
+        public WithIdView Parent { get; set; }
+    }
+
+    public class NestedObjectView : WithIdView
+    {
+        public ShallowObjectView Parent { get; set; }
+    }
+}
